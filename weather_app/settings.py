31b70ac66eeb8e6116837b8f1f5d129704bccea0
Vaalenok <130597988+Vaalenok@users.noreply.github.com>
2025-05-31 20:55:02 +0500

    Update settings.py

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f-gns&%s8hqq$xkmq^qk2xs4acayw^_m4j)iw1n1^-(1c51--1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
